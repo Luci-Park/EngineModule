4e0733348828376ec45d31c5f2ef1707afed4d29
Sumin Park <lucipark05@gmail.com>
2026-07-03 22:55:27 -0700

    PCH

--- cmake/EngineModule.cmake
+++ cmake/EngineModule.cmake
@@ -1,3 +1,15 @@
+set(ENGINE_PCH_COMMON_HEADERS
+    <vector> <string> <memory> <unordered_map> <functional> <algorithm> <cstdint>
+)
+
+function(engine_target_pch target)
+    # ARGN = extra headers a specific target wants beyond the common set
+    target_precompile_headers(${target} PRIVATE
+        ${ENGINE_PCH_COMMON_HEADERS}
+        ${ARGN}
+    )
+endfunction()
+
 function(engine_add_module name)
     set(target engine_${name})
     add_library(${target} STATIC ${ARGN})
@@ -10,6 +22,7 @@ function(engine_add_module name)
 
     target_compile_features(${target} PUBLIC cxx_std_20)
     engine_set_warnings(${target})
+    engine_target_pch(${target})
 
     set_target_properties(${target} PROPERTIES FOLDER "modules")
 endfunction()
