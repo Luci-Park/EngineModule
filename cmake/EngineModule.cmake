62a672edd98f35c3f0f75525668efe47b1243538
Sumin Park <lucipark05@gmail.com>
2026-07-03 23:27:55 -0700

    Cmake setup for platform and .dll

--- cmake/EngineModule.cmake
+++ cmake/EngineModule.cmake
@@ -12,7 +12,13 @@ endfunction()
 
 function(engine_add_module name)
     set(target engine_${name})
-    add_library(${target} STATIC ${ARGN})
+    
+    if(ENGINE_SHARED)
+        add_library(${target} SHARED ${ARGN})
+    else()
+        add_library(${target} STATIC ${ARGN})
+    endif()
+
     add_library(engine::${name} ALIAS ${target})
 
     target_include_directories(${target}
@@ -24,5 +30,21 @@ function(engine_add_module name)
     engine_set_warnings(${target})
     engine_target_pch(${target})
 
-    set_target_properties(${target} PROPERTIES FOLDER "modules")
+    string(TOUPPER ${name} name_upper)
+
+    include(GenerateExportHeader)
+    generate_export_header(${target}
+        BASE_NAME ${name}
+        EXPORT_MACRO_NAME ENGINE_${name_upper}_API
+        EXPORT_FILE_NAME ${CMAKE_CURRENT_BINARY_DIR}/include/engine/${name}/${name}_export.h
+    )
+    target_include_directories(${target}
+        PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>
+    )
+
+    set_target_properties(${target} PROPERTIES 
+        FOLDER "modules"
+        CXX_VISIBILITY_PRESET hidden
+        VISIBILITY_INLINES_HIDDEN ON
+        )
 endfunction()
